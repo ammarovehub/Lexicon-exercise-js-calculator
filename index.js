31239ad62c0ebd04388485f801194625f1f6994f
ammarovehub <ammarove1@pm.me>
2024-07-05 11:53:09 +0200

    Added the operator prompt.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-alert("Welcome to your brand new calculator")
+alert("Welcome to your brand new calculator!")
 
 // Ask about the first number
-let fNumber = Number(prompt("Enter the first number"))
+let fNumber = Number(prompt("Enter the first number."))
 
 // Ask about the second number
-let sNumber = prompt("enter another number")
+let sNumber = prompt("Enter another number.")
+
+// Ask about an operator 
+let operator = prompt("please select one of the following operators (+, -, *, /).")
 
 alert("Take care")
