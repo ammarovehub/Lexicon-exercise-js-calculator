b41809966bf5a1ac7863f4caaf65fa0541a4bfbd
ammarovehub <ammarove1@pm.me>
2024-07-05 11:42:23 +0200

    Added the first 2 prompts, used differnt ways for learning purposes.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 alert("Welcome to your brand new calculator")
+
+// Ask about the first number
+let fNumber = Number(prompt("Enter the first number"))
+
+// Ask about the second number
+let sNumber = prompt("enter another number")
+
 alert("Take care")
