8028e7155df9a87cd77fc46d0848d316b42ec06b
ammarovehub <ammarove1@pm.me>
2024-07-05 13:47:03 +0200

    Added the + and - operations but getting illogical results.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 alert("Welcome to your brand new calculator!")
 
 // Ask about the first number
-let fNumber = Number(prompt("Enter the first number."))
+let fNumber = prompt("Enter the first number.");
 
 // Ask about the second number
-let sNumber = prompt("Enter another number.")
+let sNumber = prompt("Enter another number.");
 
 // Ask about an operator 
-let operator = prompt("please select one of the following operators (+, -, *, /).")
+let operator = prompt("please select one of the following operators (+, -, *, /).");
+
+// Creating a new result variable
+let result;
+
+// The mathematical operation
+if (operator === "+") {
+    result = fNumber + sNumber;
+    alert(`The result of ${fNumber} + ${sNumber} is ${result}`);
+}
+else if (operator === "-") {
+    result = fNumber - sNumber;
+    alert(`The result of ${fNumber} - ${sNumber} is ${result}`)
+}
+
+else if (
+    alert("Please use the correct operator.")
+)
 
 alert("Take care")
